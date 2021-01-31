7789db4ffe368bc1ee65534089d31a333c1061a6
kaufmjoh <kaufmjoh@oregonstate.edu>
2021-01-30 21:52:30 -0800

    Version without error handling

--- john_kaufman_hw1.py
+++ john_kaufman_hw1.py
@@ -1,6 +1,6 @@
 #This program can be run on the OSU flip servers by entering 'python john_kaufman_hw1.py' (also works with other versions of python)
 
-#John Kaufman CS 362 Winter 2020 HW1: Python Leap Year Calculator
+#John Kaufman CS 362 Winter 2020 HW1: Python Leap Year Calculator (WITHOUT ERROR HANDLING)
 #User enters a year, this returns whether the year is a leap year or not
 
 #Leap year rules: divisible by 4, but not by 100, unless also by 400.
